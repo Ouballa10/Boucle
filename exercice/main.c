8b05b8143af54db4bb1cdf21757a1a5b6a066933
ABDELMOUNAIM <abdelmounaimoub12@gmail.com>
2024-09-28 10:38:56 +0200

    v2

---
+++ exercice/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+int N ;
+int i;
+printf("dkhel n");
+scanf("%d",&N);
+
+// Print the multiplication table for the number 2
+for (i = 1; i <= 10; i++) {
+  printf("%d x %d = %d\n", N, i, N * i);
+}
+
+return 0;
+
+
+    return 0;
+}
